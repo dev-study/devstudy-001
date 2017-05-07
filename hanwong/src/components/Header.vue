d6f3fa4c503de6ebd897b2869e2ddeb0092bbcf7
Kim Han Woong <hanationbear@gmail.com>
2017-05-07 21:59:04 +0900

    title 수정

--- hanwong/src/components/Header.vue
+++ hanwong/src/components/Header.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-default">
     <div class="container-fluid">
       <div class="navbar-header">
-        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+        <router-link to="/" class="navbar-brand">Hanwong</router-link>
       </div>
 
       <div class="collapse navbar-collapse" >
